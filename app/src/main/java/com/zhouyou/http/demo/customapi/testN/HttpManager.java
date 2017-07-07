40f6771a44e351c52529604aedd9e769bd2cf389
zhou-you <zhouyou1989123@gmail.com>
2017-07-07 11:44:30 +0800

    根据自定义请求写一个请求的管理类方便调用

--- app/src/main/java/com/zhouyou/http/demo/customapi/testN/HttpManager.java
+++ app/src/main/java/com/zhouyou/http/demo/customapi/testN/HttpManager.java
@@ -20,7 +20,7 @@ import com.zhouyou.http.request.GetRequest;
 import com.zhouyou.http.request.PostRequest;
 
 /**
- * <p>描述：（这里用一句话描述这个类的作用)</p>
+ * <p>描述：根据自定义请求写一个请求的管理类方便调用</p>
  * 作者： zhouyou<br>
  * 日期： 2017/7/7 10:31 <br>
  * 版本： v1.0<br>
